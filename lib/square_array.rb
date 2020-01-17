a25d9eaf2cef27d8f455a0957d756787235e2ead
Daniel Succar <danielsuccar@icloud.com>
2020-01-17 20:14:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
    counter = 0
 
    while counter < array.length do
-     puts array[counter]**2 = array[counter]
+     puts array[counter]**2 == array[counter]
       counter += 1
 end
 end
